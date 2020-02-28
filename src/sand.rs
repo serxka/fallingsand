0b5915b7c3205470ed490fc2617ac80e5f8861b9
Milo Wheeler <milowheeler@protonmail.com>
2020-02-29 00:08:22 +1100

    changed sand colour

--- src/sand.rs
+++ src/sand.rs
@@ -108,7 +108,7 @@ impl World {
                 let xi = x / 8;
                 let yi = y / 8;
                 let c = self.get_cell(xi ,yi);
-                let colour: u32 = match c.species {Species::Empty => {0xFF_FF_FF},Species::Wall => {0x00_FF_FF},Species::Sand => {0x00_00_00},};
+                let colour: u32 = match c.species {Species::Empty => {0xFF_FF_FF},Species::Wall => {0x00_FF_FF},Species::Sand => {0xEDC9AF},};
                 let idx = (y as usize)*pitch + (x as usize)*3;
                 buffer[idx] = ((colour & 0xFF_00_00) >> 16) as u8;
                 buffer[idx + 1] = ((colour & 0xFF_00) >> 8) as u8;
